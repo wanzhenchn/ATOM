27159516871a461385e4deb9a690a9af4bdeaea0
wuhuikx <hattie.wu@amd.com>
2026-03-14 03:22:01 -0500

    Change the build.sh

--- scripts/private/build.sh
+++ scripts/private/build.sh
@@ -1,12 +1,15 @@
-pip uninstall -y aiter
-cd aiter
+cd /home/hatwu/vllm
+pip install -r requirements/rocm-build.txt
 python3 setup.py develop
-cd ..
 
-pip install --upgrade triton
-pip uninstall -u transformers
-pip install transformers==5.0.0
+pip uninstall -y aiter
+cd /home/hatwu/aiter
+python3 setup.py develop
+
+#pip install --upgrade triton
+#pip uninstall -u transformers
+#pip install transformers==5.0.0
 pip install git+https://github.com/foundation-model-stack/fastsafetensors.git
 
-cd ATOM
+cd /home/hatwu/ATOM
 pip install -e . 2>&1 | tee build.log
